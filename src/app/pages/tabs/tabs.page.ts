e6e40c1991879a7213de3494b81c4fcab3bd0b24
danscobaba <danakin45@gmail.com>
2023-04-27 20:19:19 +0100

    update

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
+  animations: [
+    trigger('fadeAnimation', [
+      state('tab-show', style({ opacity: 1, fontWeight: 900 })),
+      state('tab-hide', style({ opacity: 0.3 })),
+      transition('tab-hide => tab-show', animate('300ms ease-in')),
+      transition('tab-show => tab-hide', animate('300ms ease-out'))
+    ])
+  ]
 })
 export class TabsPage implements OnInit {
+tabSelected: any = 'dashboard';
+onTabSelected(item) {
+console.log(item);
+this.tabSelected = item;
+// if(item == 'discover'){
+//   alert('working');
+// }
+}
 
-  constructor() { }
+  constructor() {
+    // let g = window.location.href;
+    // console.log( g.valueOf());
+   
+   }
 
   ngOnInit() {
   }
